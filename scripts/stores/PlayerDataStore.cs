890c9f1691364223219d60aafec390271a49315e
Chris Margroff <sora3087@gmail.com>
2025-05-02 16:38:38 -0400

    set player data default to encrypted

--- scripts/stores/PlayerDataStore.cs
+++ scripts/stores/PlayerDataStore.cs
@@ -3,6 +3,7 @@ namespace JamTemplate.Stores;
 
 public partial class PlayerDataStore : BaseSavedStore
 {
+  public new bool Encrypted = true;
   public string PlayerName = "Player";
   public int Hp = 100;
 }
