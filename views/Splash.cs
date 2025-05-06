2635e4699f04ec0ed56500536dd3fc80e2446684
Chris Margroff <sora3087@gmail.com>
2025-05-06 18:40:29 -0400

    splash dismiss after 5s

--- views/Splash.cs
+++ views/Splash.cs
@@ -1,7 +1,18 @@
 using Godot;
-using System;
+using JamTemplate.Managers;
+using JamTemplate.Util;
 
 namespace JamTemplate.Views;
 public partial class Splash : Control
 {
+  private SceneManager _sceneManager;
+  [FromServices]
+  public void Inject(SceneManager sceneManager)
+  {
+    _sceneManager = sceneManager;
+  }
+  public void Timeout()
+  {
+    _sceneManager.ChangeScene("Title");
+  }
 }
