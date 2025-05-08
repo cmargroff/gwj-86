2aa341d9ac4761d829b87a2c986c0c98f91d5521
Chris Margroff <sora3087@gmail.com>
2025-05-07 22:16:29 -0400

    exit credits to title screen

--- views/Credits.cs
+++ views/Credits.cs
@@ -1,16 +1,31 @@
 using Godot;
+using JamTemplate.Managers;
+using JamTemplate.Util;
 
 public partial class Credits : Control
 {
   [Export]
   public string CopyResourcePath;
+  private SceneManager _sceneManager;
+  [FromServices]
+  public void Inject(SceneManager sceneManager)
+  {
+    _sceneManager = sceneManager;
+  }
   public override void _EnterTree()
   {
     if (CopyResourcePath != null)
     {
       var handle = FileAccess.Open(CopyResourcePath, FileAccess.ModeFlags.Read);
       var copy = handle.GetAsText();
-      GetNode<RichTextLabel>("%BodyCopy").Text = copy;
+      GetNode<RichTextLabel>("%BodyCopy").Text = copy + "\r\n\r\n\r\n[center]Press any key to go back[/center]";
+    }
+  }
+  public override void _Input(InputEvent @event)
+  {
+    if (@event is InputEventKey keyEvent && keyEvent.IsPressed())
+    {
+      _sceneManager.ChangeScene("Title");
     }
   }
 }
