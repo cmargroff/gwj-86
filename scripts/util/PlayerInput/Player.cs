68990d2d8d4ecb7f2c453d4c1c31f06186cb8b97
Chris Margroff <sora3087@gmail.com>
2025-05-20 14:44:20 -0400

    relocate player input code

--- scripts/util/PlayerInput/Player.cs
+++ scripts/util/PlayerInput/Player.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using Godot;
 
-namespace JamTemplate.PlayerManager;
+namespace JamTemplate.Util.PlayerInput;
 
 public partial class Player
 {
--- scripts/util/PlayerInput/PlayerManager.cs
+++ scripts/util/PlayerInput/PlayerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
-namespace JamTemplate.PlayerManager;
+namespace JamTemplate.Util.PlayerInput;
 
 public partial class PlayerManager
 {
