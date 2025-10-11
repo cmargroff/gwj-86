5832e8a73de72496f55b5e9ad7e39e2c8b517cc4
Chris Margroff <sora3087@gmail.com>
2025-10-11 17:39:35 -0400

    common state stubs

---
+++ components/player/states/common/DashState.cs
@@ -0,0 +1,8 @@
+using JamTemplate.Util.FSM;
+
+namespace JamTemplate.Player.States;
+
+public class DashState() : AnimatedState
+{
+
+}
---
+++ components/player/states/common/EntryState.cs
@@ -0,0 +1,8 @@
+using JamTemplate.Util.FSM;
+
+namespace JamTemplate.Player.States;
+
+public class EntryState() : AnimatedState
+{
+
+}
