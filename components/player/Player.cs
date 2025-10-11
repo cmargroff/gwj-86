1e71563a1befc961571a8718e47cccc6165ad72b
hakalochka <108027001+hakalochka@users.noreply.github.com>
2025-10-11 16:52:05 +0200

    added player scene

---
+++ components/player/Player.cs
@@ -0,0 +1,47 @@
+using Godot;
+using JamTemplate;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float JumpVelocity = -300.0f;
+	private StatsManager _statsManager;
+
+    public override void _EnterTree()
+    {
+		_statsManager = Globals.ServiceProvider.GetRequiredService<StatsManager>();
+    }
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * _statsManager.Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, _statsManager.Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- scripts/managers/StatsManager.cs
+++ scripts/managers/StatsManager.cs
@@ -4,6 +4,6 @@ public class StatsManager {
   public float Health { get; set; }
   public float Will { get; set; }
   public float Strength { get; set; }
-  public float Speed { get; set; }
+  public float Speed { get; set; } = 300.0f;
   public float JumpHeight { get; set; }
 }
