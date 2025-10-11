f0cce184f516c0b995f09e1b1aaf0ea103b0b336
Chris Margroff <sora3087@gmail.com>
2025-10-11 14:12:10 -0400

    update FSM core code

--- scripts/util/FSM/AnimatedState.cs
+++ scripts/util/FSM/AnimatedState.cs
@@ -4,6 +4,9 @@ namespace JamTemplate.Util.FSM;
 
 public partial class AnimatedState : State
 {
+  public AnimatedFiniteStateManager AFSM;
   public float AnimationLength;
   public Animation.LoopModeEnum AnimationLoopMode;
 }
+
+public class AnimatedState<T> : AnimatedState { }
