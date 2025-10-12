e9fc173bb0e1165a359cc1f49212305bb5134de6
Chris Margroff <sora3087@gmail.com>
2025-10-12 02:27:23 -0400

    add player control through FSM

--- components/player/Player.cs
+++ components/player/Player.cs
@@ -1,46 +1,102 @@
 using Godot;
-using JamTemplate;
+using JamTemplate.Player.States;
+using JamTemplate.Util.FSM;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+
+namespace JamTemplate.Components.Player;
 
 public partial class Player : CharacterBody2D
 {
+	public float FrictionCoefficient { get; set; } = 0.15f; // TODO: move to stats
+	public Vector2 MoveVelocity = Vector2.Zero;
+	[Export]
+	public AnimationPlayer AnimationPlayer;
 	private StatsManager _statsManager;
+	private AnimatedFiniteStateManager _fsm;
+	private uint _jumpsLeft = 0;
 
-    public override void _EnterTree()
-    {
+	public override void _EnterTree()
+	{
 		_statsManager = Globals.ServiceProvider.GetRequiredService<StatsManager>();
-    }
+		ResetJumps();
+		BuildFSM();
+	}
+	public void BuildFSM()
+	{
+		_fsm = new();
+		var states = new ServiceCollection();
+		states.AddSingleton(this);
+		states.AddSingleton(AnimationPlayer);
+		states.AddSingleton(_statsManager);
+		states.Scan(scan => scan
+			.FromAssemblyOf<EntryState>()
+			.AddClasses(classes =>
+				classes
+				.InNamespaceOf<EntryState>()
+				.AssignableTo<AnimatedState>()
+			)
+			.As<AnimatedState>()
+		);
+		_fsm.AnimationPlayer = AnimationPlayer;
+		_fsm.AddStates(states.BuildServiceProvider());
+	}
+	public override void _Ready()
+	{
+		_fsm.InitialStateName = "entry";
+		_fsm.Start();
+	}
+
+	public Vector2 GetMoveVector()
+	{
+		return Input.GetVector("move_left", "move_right", "move_up", "move_down");
+	}
+	public void Move(double delta, Vector2 moveVector, float speed)
+	{
+		MoveVelocity.X = moveVector.X * speed;
+	}
+
+	public void ResetJumps()
+	{
+		_jumpsLeft = _statsManager.MaxJumps;
+	}
+	public void DecrementJumps()
+	{
+		_jumpsLeft--;
+	}
+
+	public bool CanJump()
+	{
+		return _jumpsLeft > 0;
+	}
+	public void ResetVelocity()
+	{
+		MoveVelocity = Vector2.Zero;
+	}
+
+	public void ApplyGravity(double delta)
+	{
+		var gravity = GetGravity();
+		MoveVelocity.Y += gravity.Y * (float)delta;
+	}
+	public void ApplyFriction(double delta)
+	{
+		if (IsOnFloor())
+		{
+			MoveVelocity.X = MoveVelocity.X * (1f - FrictionCoefficient);
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		_fsm.Process(delta);
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = _statsManager.JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * _statsManager.Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, _statsManager.Speed);
-		}
-
-		Velocity = velocity;
+		_fsm.PhysicsProcess(delta);
+		Velocity = MoveVelocity;
+		ApplyFriction(delta);
+		ApplyGravity(delta);
 		MoveAndSlide();
 	}
 }
