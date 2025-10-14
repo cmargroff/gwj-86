e54ff8d21755c3181b825a19b8b0db974fddba99
Chris Margroff <sora3087@gmail.com>
2025-10-14 13:07:04 -0400

    fix skill tree manager DI

--- scripts/managers/SkillTreeManager.cs
+++ scripts/managers/SkillTreeManager.cs
@@ -1,24 +1,17 @@
-using Godot;
-using JamTemplate.Util;
-using System;
 namespace JamTemplate.Managers;
 
-public partial class SkillTreeManager
+public class SkillTreeManager
 {
 	private StatsManager _statsManager;
 	private SkillsManager _skillsManager;
-
-	[FromServices]
-	public void Inject(StatsManager statsManager, SkillsManager skillsManager)
-    {
+	public SkillTreeManager(StatsManager statsManager, SkillsManager skillsManager)
+	{
 		_statsManager = statsManager;
 		_skillsManager = skillsManager;
-    }
+	}
 	public bool CheckExp(int exp)
 	{
-		
 		var check = (_statsManager.Exp >= exp) ? true : false;
 		return check;
-		
-    }
+	}
 }
